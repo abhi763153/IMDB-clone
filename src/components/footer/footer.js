426ac2688b09dd3e3cde4a59775edcc2831d1809
Abhishek Kumar Kamti <abhishekkamti723@gmail.com>
2023-02-03 00:21:26 +0530

    My first commit

---
+++ src/components/footer/footer.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "./footer.css"
+
+
+const Footer = () =>{
+
+    return(
+        <div className="Main_Div">
+            <h2 className="footer_title">Developed by Abhishek</h2>
+        </div>
+    )
+}
+
+export default Footer
